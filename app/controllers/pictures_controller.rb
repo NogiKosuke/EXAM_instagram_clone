e8112e5e5df6cf2c0ad4c97efd6dbb8553ceae4e
nogi kosuke <kskng0902@gmail.com>
2023-04-07 15:00:15 +0900

    編集機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,16 +3,33 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
     @picture = Picture.new
   end
-  
+
   def create
     @picture = current_user.pictures.build(picture_params)
     if @picture.save
       redirect_to pictures_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path
+    else
+      render :edit
     end
   end
 
