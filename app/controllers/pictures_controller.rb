4843374207f00a6062b57f254cc3887f3ea96ea5
nogi kosuke <kskng0902@gmail.com>
2023-04-07 14:43:29 +0900

    indexページの実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,7 +10,7 @@ class PicturesController < ApplicationController
   def create
     @picture = current_user.pictures.build(picture_params)
     if @picture.save
-      redirect_to new_picture_path
+      redirect_to pictures_path
     else
       render :new
     end
