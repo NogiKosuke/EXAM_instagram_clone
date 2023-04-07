523ee7db5ce9a2bff3e8396b0cbced572c9ffa09
nogi kosuke <kskng0902@gmail.com>
2023-04-07 22:11:52 +0900

    画像投稿機能の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -42,6 +42,6 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:content,:picture)
+    params.require(:picture).permit(:content, :picture, :picture_cache)
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,5 @@ class Picture < ApplicationRecord
   validates :picture, presence: true
   validates :content, presence: true
   belongs_to :user
+  mount_uploader :picture, PictureUploader
 end
